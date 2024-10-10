7e51eecdd68bf7eaaf52f5e94f1310fcf0552ba1
Munib ul hassan <46391010+munib-ul-hassan@users.noreply.github.com>
2024-10-11 01:40:12 +0500

    setup customer entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from './customer/customer.entity';
+import { CustomerController } from './customer/customer.controller';
+import { CustomerService } from './customer/customer.service';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',  // Change this to your SQL Server host if necessary
+      port: 1433,
+      username: 'your_username',
+      password: 'your_password',
+      database: 'your_database',
+      synchronize: true,  // In production, this should be set to false
+      entities: [Customer],
+      options: { enableArithAbort: true },
+    }),
+    TypeOrmModule.forFeature([Customer]),  // Register your Customer entity
+  ],
+  controllers: [CustomerController],
+  providers: [CustomerService],
 })
 export class AppModule {}
